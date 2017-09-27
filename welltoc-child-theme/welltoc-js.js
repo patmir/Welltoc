be38ca02ef7c6a3a8c808a0414589fe6adc2f9a7
patmir <miroslaw.patryk@gmail.com>
2017-09-27 16:22:33 +0200

    up-final

--- welltoc-child-theme/welltoc-js.js
+++ welltoc-child-theme/welltoc-js.js
@@ -1,5 +1,12 @@
 jQuery(document).ready(function(){
   jQuery("#welltoc-skill-projects > .sc_skills_icon").removeClass("sc_skills_icon icon-vcard").addClass("welltoc-skills-projects-icon");
-
   jQuery("#welltoc-skill-clients > .sc_skills_icon").removeClass("sc_skills_icon icon-vcard").addClass("fa fa-4x fa-smile-o welltoc-skills-clients-icon");
-    });
+// Menu language fix
+var langContainer = jQuery(".wpglobus-current-language");
+var targetLang = jQuery(".wpglobus-current-language > ul > li > a");
+var dropDown = jQuery(".wpglobus-current-language > ul");
+langContainer.children("a").remove();
+langContainer.append(targetLang[0])
+dropDown.remove();
+
+ });
